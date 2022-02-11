ea4e7dcdbb9de11696e520a90416c103283b8378
tonystarkmark89 <99468259+tonystarkmark89@users.noreply.github.com>
2022-02-11 13:04:20 +0530

    Create question5.c

---
+++ question5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float R, A;
+
+    scanf("%f", &R);
+
+    A = 3.14 * R * R;
+
+    printf("Area of the circle = %.2f\n", A);
+
+    return 0;
+}
