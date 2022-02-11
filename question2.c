fa3660137ae0dc2f69cdfabb575574953205c073
tonystarkmark89 <99468259+tonystarkmark89@users.noreply.github.com>
2022-02-11 13:06:07 +0530

    Create question2.c

---
+++ question2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int X,Y;
+    scanf("%d%d",&X,&Y);
+    if(X>Y)
+    {
+        printf("%d",X);
+    }
+    else
+    {
+        printf("%d",Y);
+    }
+    return 0;
+}
